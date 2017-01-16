3e817d3758ead59156be07e345187bd207098115
赵少辉 <zhaoshaohui@pmkoo.com>
2017-01-16 19:57:11 +0800

    pm2

---
+++ src/nls/hi-in/strings.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+
+    "FILE": "फ़ाइल",
+    "FILE_TITLE": "फाइल",
+    "DIRECTORY": "निर्देशिका",
+    "DIRECTORY_TITLE": "निर्देशिका",
+    "DIRECTORY_NAMES_LEDE": "निर्देशिका नाम",
+    "FILENAMES_LEDE": "फ़ाइलनाम",
+    "FILENAME": "फाइलनाम",
+    "DIRECTORY_NAME": "निर्देशिका नाम",
+    "ERROR_CREATING_FILE_TITLE": "फ़ाइल बनाने में त्रुटि",
+    "EXT_MODIFIED_TITLE": "बाह्य परिवर्तन",
+    "CONFIRM_FOLDER_DELETE_TITLE": "हटाने की पुष्टि करें",
+    "CONFIRM_FOLDER_DELETE": "आप सुनिश्चित हैं कि आप फ़ोल्डर &lt;span class=&#39;dialog-filename&#39;&gt;{0}&lt;/span&gt;को हटाना चाहते हैं?",
+    "FILE_DELETED_TITLE": "फ़ाइल हटायी गई",
+    "DONE": "समाप्त",
+    "OK": "ठीक",
+    "CANCEL": "रद्द करें",
+    "SAVE_AND_OVERWRITE": "अधिलेखित करें",
+    "DELETE": "मिटाएँ",
+    "BUTTON_YES": "हाँ",
+    "BUTTON_NO": "नहीं",
+    "BUTTON_NEW_RULE": "नया नियम",
+    "CMD_FILE_NEW": "नई फ़ाइल",
+    "CMD_FILE_NEW_FOLDER": "नया‌ फ़ोल्डर",
+    "CMD_FILE_RENAME": "नाम बदलें",
+    "CMD_FILE_DELETE": "मिटाएँ",
+    "CMD_CUT": "काटें",
+    "CMD_COPY": "प्रतिलिपि बनाएँ",
+    "CMD_PASTE": "चिपकाएँ",
+    "CMD_SELECT_ALL": "सभी चुनें",
+    "CMD_TOGGLE_QUICK_EDIT": "त्वरित संपादन",
+    "CMD_TOGGLE_QUICK_DOCS": "त्‍वरित दस्तावेज़",
+    "COLOR_EDITOR_CURRENT_COLOR_SWATCH_TIP": "वर्तमान रंग",
+    "COLOR_EDITOR_HEX_BUTTON_TIP": "हेक्स प्रारूप",
+    "CMD_ENABLE_QUICK_VIEW": "हॉवर पर त्वरित दृश्य",
+    "DOCS_MORE_LINK": "और पढ़ें",
+    "UPLOAD_FILES_DIALOG_HEADER": "फ़ाइलें अपलोड करें",
+    "DRAG_AREA_UPLOAD_FILES_DIALOG_TEXT": "...या फ़ाइलें यहाँ खींचें.",
+    "DROP_AREA_UPLOAD_FILES_DIALOG_TEXT": "ठीक है, फ़ाइलें जारी करें!",
+    "UPLOADING_INDICATOR": "अपलोड हो रहा है...",
+    "BUTTON_FROM_YOUR_COMPUTER": "अपने कंप्यूटर से...",
+    "TAKE_A_SELFIE": "सेल्फ़ी ले...",
+    "CMD_MOVE_FILE": "इस पर जाएँ...",
+    "PROJECT_ROOT": "परियोजना आधार",
+    "PICK_A_FOLDER_TO_MOVE_TO": "एक फ़ोल्डर चुनें"
+});
---
+++ start.sh
@@ -0,0 +1,1 @@
+http-server -p 4400 --cors
